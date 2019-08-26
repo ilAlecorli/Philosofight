b6861ccf1edd50cf0c249f54bc14e55a77fce4a7
/\lecor <alle.zazie@gmail.com>
2019-08-26 17:42:50 +0200

    28/08/2019 Ho risolto il problema degli assets, ho implementato le mura dell'area ma non riesco a verificarle per NULL POINTER ERROR.

--- core/src/com/philosfight/game/utils/CameraHelper.java
+++ core/src/com/philosfight/game/utils/CameraHelper.java
@@ -1,9 +1,9 @@
 package com.philosfight.game.utils;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.philosfight.game.game.objects.AbstractGameObject;
 
 public class CameraHelper {
     private static final String TAG = CameraHelper.class.getName();
@@ -11,15 +11,15 @@ public class CameraHelper {
     private final float MAX_ZOOM_OUT = 10.0f;
     private Vector2 position;
     private float zoom;
-    private Sprite target;
+    private AbstractGameObject target;
     public CameraHelper () {
         position = new Vector2();
         zoom = 1.0f;
     }
     public void update (float deltaTime) {
         if (!hasTarget()) return;
-        position.x = target.getX() + target.getOriginX();
-        position.y = target.getY() + target.getOriginY();
+        position.x = target.position.x + target.origin.x;
+        position.y = target.position.y + target.origin.y;
     }
     public void setPosition (float x, float y) {
         this.position.set(x, y);
@@ -36,17 +36,18 @@ public class CameraHelper {
     public float getZoom () {
         return zoom;
     }
-    public void setTarget (Sprite target) {
+
+    public void setTarget (AbstractGameObject target) {
         this.target = target;
     }
 
-    public Sprite getTarget () {
+    public AbstractGameObject getTarget () {
         return target;
     }
     public boolean hasTarget () {
         return target != null;
     }
-    public boolean hasTarget (Sprite target) {
+    public boolean hasTarget (AbstractGameObject target) {
         return hasTarget() && this.target.equals(target);
     }
     public void applyTo (OrthographicCamera camera) {
