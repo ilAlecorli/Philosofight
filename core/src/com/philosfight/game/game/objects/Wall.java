5e0c6a1b13b7d1e994fdca95ac2f28e71e02dbfc
Jackson98Tomphson <48087046+Jackson98Tomphson@users.noreply.github.com>
2019-08-29 12:28:18 +0200

    Aggiornamento degli attributi e dei metodi di Player

--- core/src/com/philosfight/game/game/objects/Wall.java
+++ core/src/com/philosfight/game/game/objects/Wall.java
@@ -7,29 +7,29 @@ import com.philosfight.game.game.Assets;
 
 public class Wall extends AbstractGameObject {
 
-    public Wall() {
-        init();
-    }
+	public Wall() {
+		init();
+	}
 
-    private void init() {
-        dimension.set(1f,1f);
-        //origin.set(0.5f, 0.5f);
-    }
+	private void init() {
+		dimension.set(1f,1f);
+		//origin.set(0.5f, 0.5f);
+	}
 
 
-    /*
-     * texture rappresenta la texture dell'oggetto e quindi l'immagine da prendere in considerazione
-     * x e y servono a disegnare il rettangolo a determinate coordinate
-     * originX ed originY servono a dichiarare l'origine dell'oggetto, (0,0) implica l'origine nell'angolo in basso a sinistra.
-     * width e height definiscono la dimensione dell'imagine da visualizzare
-     * scaleX e scaleY definiscono la scala del rettangolo intorno all'origine
-     * rotation definisce di quanti gradi ruotare l'immagine
-     * srcX e scrY servono a "tagliare" un rettangolo, dalla texture o dal texture atlas
-     * srcHeight e srcWidth
-     * flipX e flipY specchiano l'immagine sui relativi assi.
-     * */
-    @Override
-    public void render(SpriteBatch batch) {
-        batch.draw(ObjectAssets.getTexture(), position.x, position.y, origin.x, origin.y, dimension.x, dimension.y, scale.x, scale.y, rotation, ObjectAssets.getRegionX(), ObjectAssets.getRegionY(), ObjectAssets.getRegionWidth(), ObjectAssets.getRegionHeight(), flipX, flipY);
-    }
+	/*
+	 * texture rappresenta la texture dell'oggetto e quindi l'immagine da prendere in considerazione
+	 * x e y servono a disegnare il rettangolo a determinate coordinate
+	 * originX ed originY servono a dichiarare l'origine dell'oggetto, (0,0) implica l'origine nell'angolo in basso a sinistra.
+	 * width e height definiscono la dimensione dell'imagine da visualizzare
+	 * scaleX e scaleY definiscono la scala del rettangolo intorno all'origine
+	 * rotation definisce di quanti gradi ruotare l'immagine
+	 * srcX e scrY servono a "tagliare" un rettangolo, dalla texture o dal texture atlas
+	 * srcHeight e srcWidth
+	 * flipX e flipY specchiano l'immagine sui relativi assi.
+	 * */
+	@Override
+	public void render(SpriteBatch batch) {
+		batch.draw(ObjectAssets.getTexture(), position.x, position.y, origin.x, origin.y, dimension.x, dimension.y, scale.x, scale.y, rotation, ObjectAssets.getRegionX(), ObjectAssets.getRegionY(), ObjectAssets.getRegionWidth(), ObjectAssets.getRegionHeight(), flipX, flipY);
+	}
 }
